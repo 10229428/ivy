583685890b112f6bc95b02d7972ccf6610de2830
周庆10229428 <zhou.qing4@zte.com.cn>
2020-03-23 14:40:30 +0800

    opt codes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,20 +10,7 @@ export class AppComponent {
               private cfr: ComponentFactoryResolver) {
   }
 
-  /**
-   * 这种方式，需要在模板中配置 <app-feature></app-feature> ，否则会报 找不到host 的异常
-   */
-  loadFeature() {
-    import('./feature/feature.component').then(({FeatureComponent}) => {
-      ɵrenderComponent(FeatureComponent);
-    });
-  }
-
-  /**
-   * 可以将模板中的 <app-feature></app-feature> 注释掉，配置host的方式加载
-   * 这种方式，会将上面的按钮替换掉，应该是整个页面都替换了
-   */
-  loadFeatureWithHost() {
+  loadFeatureComponent() {
     import('./feature/feature.component').then(({FeatureComponent}) => {
       ɵrenderComponent(FeatureComponent, {host: 'app-root', injector: this.injector});
     });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,13 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {InjectionToken, NgModule} from '@angular/core';
 import {AppComponent} from './app.component';
-import {AppRouterModule} from "./app-routing.module";
-import { LazyComponent } from './lazy/lazy.component';
 
 export const APP_NAME: InjectionToken<string> = new InjectionToken<string>('App Name');
 
 @NgModule({
-  declarations: [AppComponent, LazyComponent],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule, AppRouterModule
+    BrowserModule
   ],
   providers: [{
     provide: APP_NAME,
