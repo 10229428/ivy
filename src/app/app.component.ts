cea0a566c3e4ff0a51f049c5ca9586e2c4441f08
周庆10229428 <zhou.qing4@zte.com.cn>
2020-03-23 14:34:40 +0800

    add lazy load component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
-import {Component, Injector, ɵrenderComponent} from '@angular/core';
+import {Component, ComponentFactoryResolver, Injector, ViewContainerRef, ɵrenderComponent} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
 })
 export class AppComponent {
-  constructor(private injector: Injector) {
+  constructor(private injector: Injector,
+              private viewContainer: ViewContainerRef,
+              private cfr: ComponentFactoryResolver) {
   }
 
   /**
@@ -27,5 +29,22 @@ export class AppComponent {
     });
   }
 
+  async lazyLoad() {
+    const {LazyComponent} = await import('./lazy/lazy.component');
+    this.viewContainer.createComponent(this.cfr.resolveComponentFactory(LazyComponent));
+  }
 
 }
+
+
+
+
+/*
+// 清理并新建一个component，用于lazy load
+git clean -f && git co -f .
+// ng g c Lazy -m Lazy -t -s
+ng g c Lazy
+ng serve --port 4111
+const {LazyComponent} = await import('./lazy/lazy.component');
+this.vcr.createComponent(this.cfr.resolveComponentFactory(LazyComponent));
+*/
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,12 @@ import {BrowserModule} from '@angular/platform-browser';
 import {InjectionToken, NgModule} from '@angular/core';
 import {AppComponent} from './app.component';
 import {AppRouterModule} from "./app-routing.module";
+import { LazyComponent } from './lazy/lazy.component';
 
 export const APP_NAME: InjectionToken<string> = new InjectionToken<string>('App Name');
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, LazyComponent],
   imports: [
     BrowserModule, AppRouterModule
   ],
