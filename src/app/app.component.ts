0d91cf697c910ac7e1156d7727f5ec69db31cc2e
周庆10229428 <zhou.qing4@zte.com.cn>
2020-03-23 10:44:54 +0800

    hoc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,10 @@
-import {Component, Injector, ɵrenderComponent} from '@angular/core';
+import {Component, INJECTOR, Injector, ɵrenderComponent, ɵɵdirectiveInject} from '@angular/core';
 
+@LazyLoadDecorator({
+  path: './feature/feature.component',
+  component: 'FeatureComponent',
+  host: 'app-root'
+})
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -26,4 +31,40 @@ export class AppComponent {
       ɵrenderComponent(FeatureComponent, {host: 'app-root', injector: this.injector});
     });
   }
+
+  afterViewLoad() {
+    console.log('AppComponent =====> Lazy HOC loaded!');
+  }
 }
+
+export function LazyLoadDecorator(config: { path: string, component: string, host: string }) {
+  return (cmpType) => {
+    const originalFactory = cmpType.ngComponentDef.factory;
+    cmpType.ngComponentDef.factory = (...args) => {
+      const cmp = originalFactory(...args);
+      console.log('LazyLoadComponent =====> cmp: ', cmp);
+      const injector = ɵɵdirectiveInject(INJECTOR);
+      import(`${config.path}`).then(m => {
+        console.log('LazyLoadComponent =====> m: ', m);
+        ɵrenderComponent(m[config.component], {host: config.host, injector});
+      });
+      if (cmp.afterViewLoad) {
+        cmp.afterViewLoad();
+      }
+      return cmp;
+    };
+    return cmpType;
+  };
+}
+
+export function HOC() {
+  return (cmpType) => {
+    const originalFactory = cmpType.ngComponentDef.factory;
+    cmpType.ngComponentDef.factory = (...args) => {
+      const cmp = originalFactory(...args);
+      console.log(cmp);
+      return cmp;
+    };
+  };
+}
+
